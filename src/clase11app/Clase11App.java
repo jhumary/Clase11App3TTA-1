a1d69fe37d04d6ed3a3e2d0a9ede83fd0436a492
profesor-203 <profesor-203@PC-DC-203.iberotec.edu.pe>
2015-06-13 16:07:49 -0500

    Segundo Cambio

--- src/clase11app/Clase11App.java
+++ src/clase11app/Clase11App.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package clase11app;
 
 /**
@@ -16,7 +10,9 @@ public class Clase11App {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Juan");
+        
     }
     
 }
